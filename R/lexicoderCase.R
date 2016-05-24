4739b56485662a53ac7601bad1a82563650dbcd1
Simon Kiss <sjkiss@gmail.com>
2016-05-24 14:45:42 -0400

    Added tm:: to two functions for importation

--- R/lexicoderCase.R
+++ R/lexicoderCase.R
@@ -1,1 +1,1 @@
-lexicoderCase<-content_transformer(function(x) stri_replace_all_regex(x, caps$replace, caps$with, vectorize_all=F))
+lexicoderCase<-tm::content_transformer(function(x) stri_replace_all_regex(x, caps$replace, caps$with, vectorize_all=F))
--- R/lexicoderPunct.R
+++ R/lexicoderPunct.R
@@ -1,1 +1,1 @@
-lexicoderPunct<-content_transformer(function(x,y) stri_replace_all_regex(x, punct$replace, punct$with, vectorize_all=FALSE, case_insensitive=F))
+lexicoderPunct<-tm::content_transformer(function(x,y) stri_replace_all_regex(x, punct$replace, punct$with, vectorize_all=FALSE, case_insensitive=F))
